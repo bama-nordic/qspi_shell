6444db5c5b31c5b0f6eb8467959f2c0a45236843
Bansidhar PM <bansidhar.mangalwedhekar@nordicsemi.no>
2022-05-08 08:57:44 +0530

    some cleaning up

--- scripts/wifiutils.py
+++ scripts/wifiutils.py
@@ -4,7 +4,7 @@ import serial
 class WiFiUtilsClient():
     """Client class to communicate with DUT via Serial"""
 
-    def __init__(self, port='/dev/ttyACM2', baudrate=115200):
+    def __init__(self, port='/dev/ttyACM1', baudrate=115200):
         self.port = port
         self.baudrate = baudrate
 
@@ -28,7 +28,7 @@ class WiFiUtilsClient():
 
     def close(self) -> None:
         """Close connection with the device"""
-        print('Closing Serial connection')
+        #print('Closing Serial connection')
         self.ser.close()
 
     def execute_command(self, cmd: str, timeout=10):
@@ -60,7 +60,6 @@ class WiFiUtilsClient():
     def read_wrd(self,addr):
         self.execute_command(f'wifiutils read_wrd  {addr}')
         rx = self.read().decode().split("\r\n")[-3]
-        #rx = self.read().decode()
         print(rx)
 
     def write_wrd(self,addr,data):
@@ -71,9 +70,10 @@ class WiFiUtilsClient():
 
     def read_blk(self,addr,wrd_len):
         self.execute_command(f'wifiutils read_blk  {addr} {wrd_len}')
-        #rx = self.read().decode().split("\r\n")
-        rx = self.read().decode()
-        print(rx)
+        lines = int(wrd_len/4)
+        num_lines = lines if ((wrd_len%4) == 0) else lines+1
+        rx = self.read().decode().split("\r\n")[-(num_lines+1):-1]
+        print("\n".join(rx))
 
     def wifi_on(self):
         self.execute_command(f'wifiutils wifi_on')
@@ -105,7 +105,7 @@ class WiFiUtilsClient():
 
     def memtest(self,addr,pattern,incr,wrd_len):
         self.execute_command(f'wifiutils memtest  {addr} {pattern} {incr} {wrd_len}')
-        rx = self.read().decode()
+        rx = self.read().decode().split("\r\n")[-2]
         print(rx)
 
 
@@ -114,9 +114,10 @@ if __name__ == '__main__':
 
     myser =  WiFiUtilsClient()
     status = myser.connect()
-    #myser.write_blk(0x0c0000, 0xaabbccdd, 0, 16)
-    #myser.read_blk(0x0c0000, 16)
-    myser.memtest(0x0c0000, 0x0, 0x1, 69)
+    myser.write_blk(0x0c0000, 0xaabbccdd, 0, 16)
+    myser.read_blk(0x0c0000, 16)
+    myser.memtest(0x0c0000, 0x0, 0x1, 64)
+    myser.read_blk(0x0c0000,64)
 
     myser.close()
 
